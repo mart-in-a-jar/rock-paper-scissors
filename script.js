f40241717702a791f44ec7ae616efc6516cb4057
Martin <Martin@Jarneid.no>
2022-02-24 10:00:41 +0100

    Remove unused function and clean up function to validate user input

--- script.js
+++ script.js
@@ -8,45 +8,25 @@ function random(num1, num2) {
     return Math.floor(Math.random() * (num2 - (num1 - 1))) + num1;
 }
 
-// Function to generate a random number 100 times and check against the range specified (num1-num2)
-function testRandom(num1, num2) {
-    let possibleNumbers = [];
-    let checkOk = true;
-    for (let i = num1; i <= num2; i++) {
-        possibleNumbers.push(i);
-    }
-    let j;
-    for (let i = 0; i < 100; i++) {
-        j = random(num1, num2);
-        if (!possibleNumbers.includes(j)) {
-            console.log(`Random number was ${j}. This is not in the range specified!`)
-            checkOk = false;
-        }
-    }
-    if (checkOk === true) {
-        console.log("Everything works as intended.")
-    }
-}
-
 // function to generate computers choice
 function computerPlay() {
     return choices[random(0, choices.length - 1)]
 }
 
-// get player's choice
+// get and validate player's choice
 function playerPlay() {
-    let playerChoice = prompt("Choose your weapon");
-    while (playerChoice != null && !choices.includes(playerChoice.toLowerCase())) {
-        alert("Not a valid choice!");
+    let playerChoice;
+    while (true) {
         playerChoice = prompt("Choose your weapon");
+        if (playerChoice === null) {
+            return playerChoice;
+        } else if (choices.includes(playerChoice.toLowerCase())) {
+            return playerChoice.toLowerCase();
+        } else {
+        alert("Not a valid choice!");
+        }
     }
-    if (playerChoice === null) {
-        return playerChoice;
-    } else {
-        return playerChoice.toLowerCase();
-    }
-}
-
+} 
 
 // function to play a round
 function playRound(playerChoice, computerChoice) {
